e4e29776eb5793d2f533b3ce0226f0f9861bb53c
PeeplesKaitlyn-FS <162365288+PeeplesKaitlyn-FS@users.noreply.github.com>
2025-03-09 01:12:33 -0500

    Update api.js

--- server/api.js
+++ server/api.js
@@ -1,8 +1,14 @@
 const express = require('express');
+const dotenv = require('dotenv');
 const app = express();
 
+dotenv.config();
+
 app.get('/api/test', (req, res) => {
-  res.send('Hello from the API!');
+  const spotifyClientId = process.env.SPOTIFY_CLIENT_ID;
+  const spotifyClientSecret = process.env.SPOTIFY_CLIENT_SECRET;
+
+  res.send(`Spotify Client ID: ${spotifyClientId}, Spotify Client Secret: ${spotifyClientSecret}`);
 });
 
 module.exports = app;
