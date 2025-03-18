ff4c1fb06587e91f7090ee47c26956f34798c993
PeeplesKaitlyn-FS <162365288+PeeplesKaitlyn-FS@users.noreply.github.com>
2025-03-18 00:15:58 -0400

    Create App.js

---
+++ client/src/App.js
@@ -0,0 +1,13 @@
+import React from 'react';
+import SearchBar from './pages/SearchBar';
+import './styles/style.css';
+
+const App = () => {
+  return (
+    <div>
+      <SearchBar />
+    </div>
+  );
+};
+
+export default App;
