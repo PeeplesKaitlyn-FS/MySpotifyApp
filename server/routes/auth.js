66602ab603e76d22c945c6f28aa40fcd494e6d0e
PeeplesKaitlyn-FS <162365288+PeeplesKaitlyn-FS@users.noreply.github.com>
2025-03-18 02:39:04 -0400

    Update auth.js

--- server/routes/auth.js
+++ server/routes/auth.js
@@ -2,14 +2,11 @@ const express = require("express");
 const passport = require("passport");
 const passportService = require('../services/passport');
 
-const protectedRoute = passport.authenticate('local', { session: false }); 
-
 const router = express.Router();
 
 const authenticationController = require("../controllers/authentication_controller");
 
 router.post("/", authenticationController.signup);
-router.post("/signin", protectedRoute, authenticationController.signin);
-
+router.post("/signin", passport.authenticate('local', { session: true }), authenticationController.signin);
 
 module.exports = router;
