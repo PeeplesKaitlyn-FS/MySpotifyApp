7138f9200c43badca5da86f294f4de8d57c20efe
PeeplesKaitlyn-FS <162365288+PeeplesKaitlyn-FS@users.noreply.github.com>
2025-03-18 02:39:02 -0400

    Update authentication_controller.js

--- server/controllers/authentication_controller.js
+++ server/controllers/authentication_controller.js
@@ -4,41 +4,49 @@ const bcrypt = require("bcrypt")
 const config = require("../config")
 
 const tokenForUser = (user) => {
-    const timestamp = new Date().getTime()
-    return jwt.encode({
-        sub: user.id, 
-        iat: timestamp
-    }, config.secret)
+  const timestamp = new Date().getTime()
+  return jwt.encode({
+    sub: user.id,
+    iat: timestamp,
+    exp: timestamp + (60 * 60 * 24 * 7) // expires in 1 week
+  }, config.secret)
 }
 
 exports.signin = async (req, res, next) => {
+  try {
     const user = req.user
-    res.send({user_id: user._id, token: tokenForUser(user)})
+    if (!user) {
+      return res.status(401).json({ message: "Invalid credentials" })
+    }
+    const isValidPassword = await bcrypt.compare(req.body.password, user.password)
+    if (!isValidPassword) {
+      return res.status(401).json({ message: "Invalid credentials" })
+    }
+    res.send({ user_id: user._id, token: tokenForUser(user) })
+  } catch (error) {
+    return next(error)
+  }
 }
 
 exports.signup = async (req, res, next) => {
-    const { email, password } = req.body
-    try {
-        const user = await User.findOne({ email })
-        if(!email || !password) {
-            return res.status(422).json({ message: "Email and password are required"})
-        }
-
-        User.findOne({email: email}), (error, existingUser) => {
-            if(error) {return next(error)}
-            if(existingUser) {return res.status(422).json({ message: "Email is already in use"})}
-            
-            const user = new User({ 
-                email: email, 
-                password: password 
-            })
-
-            user.save((error) => {
-                if(error) {return next(error)}
-                res.json({user_id: user._id, token: tokenForUser(user)})
-            })
-        }
-    } catch(error) {
-        return next(error)
+  const { email, password } = req.body
+  try {
+    if (!email || !password) {
+      return res.status(422).json({ message: "Email and password are required" })
     }
+    const isValidEmail = /^[a-zA-Z0-9._%+-]+@[a-zA-Z0-9.-]+\.[a-zA-Z]{2,}$/.test(email)
+    if (!isValidEmail) {
+      return res.status(422).json({ message: "Invalid email address" })
+    }
+    const existingUser = await User.findOne({ email })
+    if (existingUser) {
+      return res.status(422).json({ message: "Email is already in use" })
+    }
+    const hashedPassword = await bcrypt.hash(password, 10)
+    const user = new User({ email, password: hashedPassword })
+    await user.save()
+    res.json({ user_id: user._id, token: tokenForUser(user) })
+  } catch (error) {
+    return next(error)
+  }
 }
