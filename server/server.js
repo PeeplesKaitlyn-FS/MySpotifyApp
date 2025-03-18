e1be0736f6b8bf0fa16b584deb90a750dd11b63f
PeeplesKaitlyn-FS <162365288+PeeplesKaitlyn-FS@users.noreply.github.com>
2025-03-18 00:16:06 -0400

    Update server.js

--- server/server.js
+++ server/server.js
@@ -1,11 +1,12 @@
 const express = require('express');
+const path = require('path');
 const app = express();
 const port = 3000;
 
-app.use(express.static('public'));
+app.use(express.static(path.join(__dirname, '../client/public')));
 
 app.get('/', (req, res) => {
-  console.log('Server received a request');
+  res.sendFile(path.join(__dirname, '../client/public', 'index.html'));
 });
 
 app.listen(port, () => {
