b0752f34a63a9100db88838d71bf444966f78f03
PeeplesKaitlyn-FS <162365288+PeeplesKaitlyn-FS@users.noreply.github.com>
2025-03-24 22:17:02 -0400

    Update spotify_routes.js

--- server/routes/spotify_routes.js
+++ server/routes/spotify_routes.js
@@ -6,4 +6,9 @@ router.post('/signin', authenticationController.signin);
 router.get('/callback', authenticationController.callback);
 router.post('/signup', authenticationController.signup);
 
+router.get('/tracks', authenticationController.getTracks);
+module.exports = (app) => {
+    app.use('/tracks', router);
+  };
+
 module.exports = router;
