021763e32f2a30db4104f06ae1343e58d56b64a9
PeeplesKaitlyn-FS <162365288+PeeplesKaitlyn-FS@users.noreply.github.com>
2025-03-24 22:17:01 -0400

    Update auth.js

--- server/routes/auth.js
+++ server/routes/auth.js
@@ -8,5 +8,13 @@ const authenticationController = require("../controllers/authentication_controll
 
 router.post("/", authenticationController.signup);
 router.post("/signin", passport.authenticate('local', { session: true }), authenticationController.signin);
+router.get("/spotify", passport.authenticate("spotify", {
+  scope: ["user-read-email", "user-read-private"],
+  callbackURL: "http://localhost:3000/auth/spotify/callback"
+}));
+
+router.get("/spotify/callback", passport.authenticate('spotify', { failureRedirect: '/' }), (req, res) => {
+  res.redirect("/"); 
+});
 
 module.exports = router;
