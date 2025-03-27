ac3b6a9f114ac02aabcba6f0504ce4ceb24df090
PeeplesKaitlyn-FS <162365288+PeeplesKaitlyn-FS@users.noreply.github.com>
2025-03-26 21:32:54 -0400

    Update Login.js

--- client/src/pages/Login.js
+++ client/src/pages/Login.js
@@ -3,37 +3,40 @@ import { useNavigate } from 'react-router-dom';
 
 const Login = () => {
   const [accessToken, setAccessToken] = useState(null);
+  const [username, setUsername] = useState('');
+  const [password, setPassword] = useState('');
   const navigate = useNavigate();
 
-  useEffect(() => {
-    const storedToken = localStorage.getItem('access_token');
-    if (storedToken) {
-      setAccessToken(storedToken);
-    } else {
-      navigate('/login');
+  const handleLogin = async (event) => {
+    event.preventDefault();
+    try {
+      const response = await fetch('/api/login', {
+        method: 'POST',
+        headers: {
+          'Content-Type': 'application/json',
+        },
+        body: JSON.stringify({ username, password }),
+      });
+      const data = await response.json();
+      setAccessToken(data.accessToken);
+      navigate('/callback');
+    } catch (error) {
+      console.error(error);
     }
-  }, []);
-
-  const handleLogin = () => {
-    const clientId = 'your_client_id';
-    const scope = 'user-read-private user-read-email';
-    const redirectUri = 'http://localhost:3000/callback';
-    const url = `https://accounts.spotify.com/authorize?client_id=${clientId}&scope=${scope}&redirect_uri=${redirectUri}&response_type=code`;
-    window.location.href = url;
   };
 
   return (
     <div>
       <h1>Music Search App</h1>
       <p>In order to use this app you must be logged in!</p>
-      <form id="login-form">
+      <form onSubmit={handleLogin}>
         <label for="username">Username:</label>
-        <input type="text" id="username" name="username"/><br />
+        <input type="text" id="username" value={username} onChange={(event) => setUsername(event.target.value)} /><br />
         <label for="password">Password:</label>
-        <input type="password" id="password" name="password"/><br />
+        <input type="password" id="password" value={password} onChange={(event) => setPassword(event.target.value)} /><br />
         <button type="submit">Login</button>
       </form>
-      <button onClick={handleLogin}>Login with Spotify</button>
+      <button onClick={() => navigate('/callback')}>Login with Spotify</button>
     </div>
   );
 };
