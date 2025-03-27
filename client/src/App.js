2f3efd1e1b325654d1aadcb563a8edc13e893e03
PeeplesKaitlyn-FS <162365288+PeeplesKaitlyn-FS@users.noreply.github.com>
2025-03-27 00:45:33 -0400

    Update App.js

--- client/src/App.js
+++ client/src/App.js
@@ -2,15 +2,17 @@ import React from 'react';
 import { BrowserRouter, Routes, Route } from 'react-router-dom';
 import Login from './pages/Login';
 import Callback from './pages/Callback';
-import SearchBar from './pages/SearchBar';
+//import SearchBar from './pages/SearchBar';
+import Dashboard from './components/Dashboard';
 
 const App = () => {
   return (
     <BrowserRouter>
       <Routes>
+        <Route path="/dashboard" element={<Dashboard />} />
         <Route path="/login" element={<Login />} />
         <Route path="/callback" element={<Callback />} />
-        <Route path="/" element={<SearchBar />} />
+        <Route path="/" element={<Login />} />
       </Routes>
     </BrowserRouter>
   );
