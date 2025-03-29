b1b8c74b01ea8a3ebc0e81c9f0d8aff8b9a66232
PeeplesKaitlyn-FS <162365288+PeeplesKaitlyn-FS@users.noreply.github.com>
2025-03-28 21:26:51 -0400

    Update App.js

--- client/src/App.js
+++ client/src/App.js
@@ -1,9 +1,17 @@
-import React from 'react';
+import React, { useEffect } from 'react';
 import { BrowserRouter, Routes, Route } from 'react-router-dom';
 import Login from './pages/Login';
 import Callback from './pages/Callback';
 //import SearchBar from './pages/SearchBar';
 import Dashboard from './components/Dashboard';
+
+const AuthSpotify = () => {
+  useEffect(() => {
+    window.location.href = `https://accounts.spotify.com/authorize?client_id=${process.env.SPOTIFY_CLIENT_ID}&response_type=code&redirect_uri=http://localhost:3000/callback&scope=user-read-private%20user-read-email`;
+  }, []);
+
+  return <div>Redirecting to Spotify...</div>;
+};
 
 const App = () => {
   return (
@@ -13,6 +21,7 @@ const App = () => {
         <Route path="/login" element={<Login />} />
         <Route path="/callback" element={<Callback />} />
         <Route path="/" element={<Login />} />
+        <Route path="/auth/spotify" element={<AuthSpotify />} />
       </Routes>
     </BrowserRouter>
   );
