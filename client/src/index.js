626484e728980a291317558dadbdb54dd7343fea
PeeplesKaitlyn-FS <162365288+PeeplesKaitlyn-FS@users.noreply.github.com>
2025-03-28 21:26:54 -0400

    Update index.js

--- client/src/index.js
+++ client/src/index.js
@@ -1,9 +1,10 @@
 import React from 'react';
-import { createRoot } from 'react-dom/client';
-import App from './App';
-import './App.css';
+import ReactDOM from 'react-dom/client';
+import App from './app';
 
-const root = createRoot(document.getElementById('app'));
+console.log('Hello from index.js!');
+
+const root = ReactDOM.createRoot(document.getElementById('app'));
 root.render(
   <React.StrictMode>
     <App />
